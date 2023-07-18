9448fa96606724c2728eb1c0a48d3404ae303504
Francisco Suárez <paco.max@gmail.com>
2023-07-18 22:36:33 +0200

    08 loops

---
+++ examples/07_array_de_array.js
@@ -0,0 +1,12 @@
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log(matrix[1][2]);
+
+let nombre = "pepito";
+nombre = nombre.replace(nombre[0], nombre[0].toUpperCase())
+
+console.log(nombre)
