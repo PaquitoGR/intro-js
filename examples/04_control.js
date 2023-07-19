81ec75151c75e126ea6fa55a8504d958820a5b3e
Francisco Suárez <paco.max@gmail.com>
2023-07-19 12:05:07 +0200

    04 control-Switch

--- examples/04_control.js
+++ examples/04_control.js
@@ -24,3 +24,50 @@ const resultado = edad === 30; // true o un false
 const message = resultado ? 'Tienes mi edad' : 'No tienes mi edad';
 
 console.log(message);
+
+console.log('<---example--->');
+
+//valores "falsos" : '', Undefined, Null, NaN, 0
+let value = 0;
+
+if (value) {
+    console.log(`value: ${value}`);
+} else {
+    console.log('Else');
+}
+
+// switch
+
+let day = 'Sunday';
+
+if (day === 'Monday') {
+    console.log(`it's ${day}`)
+} else if (day === 'Tuesday') {
+    console.log(`it's ${day}`)
+} else if (day === 'Wednesday') {
+     console.log(`it's ${day}`)
+} else if (day === 'Thursday') {
+    console.log(`it's ${day}`)
+} else if (day === 'Friday') {
+    console.log(`it's ${day}`)
+}
+
+switch (day) {
+    case "Monday":
+        console.log(`Switch: It's ${day}`);
+        break;
+    case "Tuesday":
+        console.log(`Switch: It's ${day}`);
+        break;
+    case "Wedenesday":
+        console.log(`Switch: It's ${day}`);
+        break;
+    case "Thursday":
+        console.log(`Switch: It's ${day}`);
+        break;
+    case "Friday":
+        console.log(`Switch: It's ${day}`);
+        break;
+    default:
+        console.log("It's neither Monday, Tuesday, Wednesday, Thursday nor Friday")
+}
---
+++ examples/08_loops.js
@@ -0,0 +1,13 @@
+// for
+
+let count = [];
+console.log('Inicio');
+for (let i = 0; i < 5; i++) {
+    console.log('Bloque dentro del for');
+    count.push(i);
+}
+console.log(count)
+console.log('FIN');
+
+// While
+
