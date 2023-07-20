3a14ff6b11f794d80c8438bbe6c1983a2cd9c8cd
Francisco Suárez <paco.max@gmail.com>
2023-07-20 19:32:25 +0200

    08 loops - continue/breeak

--- examples/08_loops.js
+++ examples/08_loops.js
@@ -1,13 +1,42 @@
 // for
 
-let count = [];
-console.log('Inicio');
-for (let i = 0; i < 5; i++) {
-    console.log('Bloque dentro del for');
-    count.push(i);
+// let count = [];
+// console.log('Inicio');
+// for (let i = 0; i < 5; i++) {
+//     console.log('Bloque dentro del for');
+//     count.push(i);
+// }
+
+// console.log('FIN',count);
+
+const fruit = ['apple', 'banana', 'kiwi', 'watermelon', 'orange', 'Kiwi'];
+
+for (let i = 0; i < fruit.lenght; i++) {
+    const fruit = fruit[i];
+    if (fruit.toLowerCase() !== 'kiwi') {
+        console.log(fruit);
+    } else {
+        console.log("****");
+    }
 }
-console.log(count)
-console.log('FIN');
+
+console.log('---Continue y break---')
+
+for (let index = 0; index < 10; index ++) {
+    // continue
+    if (index === 2 || index === 0) {
+        continue;
+    }
+    console.log('index', index)
+}
+
+for (let index = 0; index < 10; index ++) {
+    // break
+    if (index === 2 || index === 0) {
+        continue;
+    }
+    console.log('index', index)
+}
 
 // While
 
