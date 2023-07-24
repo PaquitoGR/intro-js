ccce67d5ec7c658f873c2f32df7616ec57a08d36
Francisco Suárez <paco.max@gmail.com>
2023-07-24 22:16:33 +0200

    10 functions - arrow functions fin

--- examples/10_functions.js
+++ examples/10_functions.js
@@ -43,4 +43,49 @@ const saludoPersona = crearSaludo;
 console.log(saludoPersona("Pepitilla"));
 
 console.log(typeof(crearSaludo)); // function
-console.log(typeof(crearSaludo("Pepis")));
+console.log(typeof(crearSaludo("Pepis"))); //  String
+
+const nums = [1, 2, 33, 41, 5, 60, 74, 87, 90, 101];
+function oddNumers(numbers) {
+  let oddNums = [];
+  for (let num of numbers) {
+    if (num % 2 !== 0) oddNums.push(num);     
+  }
+  return oddNums;
+}
+
+console.log(oddNumers(nums))
+
+// arrow functions
+
+/*
+const restar = (num1 = 0, num2 = 0) => {
+  return num1 - num2;
+}
+*/
+
+const restar = (num1 = 0, num2 = 0) => num1 - num2;
+
+console.log(restar(10, 80));
+
+const activateAccount = (name) => {
+  return {
+    // name: name,
+    name, // <-- si la key y la variable tienen el mismo nombre.
+    active: true
+  };
+}
+
+
+const activateAccount2 = name => (
+  {
+  name,
+  active: true
+  }
+)
+
+
+const user1 = activateAccount('Kevin');
+console.log(user1);
+
+console.log(activateAccount("Pepito"));
