9b458fd0a6d8288a19d4cc0f731733b1cb5675b1
Francisco Suárez <paco.max@gmail.com>
2023-07-25 20:02:20 +0200

    11 functional inicio

---
+++ examples/11_functional.js
@@ -0,0 +1,9 @@
+/*
+La programación funcional es un paradigma de
+programación que se centra en el uso de 
+funciones como bloques de construcción fundamentales.
+En el contexto de JavaScript, la
+programación funcional implica escribir códiugo
+que se basa en funciones puras, evitando el uso
+de estado mutable y efectos secundarios.
+*/
