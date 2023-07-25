b3cc9e1171f81c3c6d4c3f65a9bff9ea535b5944
Francisco Suárez <paco.max@gmail.com>
2023-07-25 20:14:50 +0200

    11 functional - inmutabilidad

--- examples/11_functional.js
+++ examples/11_functional.js
@@ -7,3 +7,42 @@ programación funcional implica escribir códiugo
 que se basa en funciones puras, evitando el uso
 de estado mutable y efectos secundarios.
 */
+
+/*
+Funciones puras: las funciones puras son 
+aquellas que siempre producen el mismo resultado
+para los mismos argumentos y no tienen efectos
+secundarios observables. No modifican variables
+fuera de su ámbito y no alteran el estado global.
+*/
+
+// Función pura
+
+const numero = 0;
+const sumar = (a, b) => a + b;
+
+sumar (2, 2) //4
+sumar (2, 2) //4
+
+// Función no pura
+let num = 0;
+const sumarImpure = (num2) => {
+    num = num + num2;
+}
+
+sumarImpure(2); // 2
+sumarImpure(2); // 4
+
+
+const datos = [];
+
+const addElement = (arr, newElement) => {
+    arr.push(newElement);
+    return arr;
+};
+
+console.log(addElement(datos, "Elemento")); // ['Elemento']
+
+console.log(addElement(datos, "Elemento")); // ['Elemento'] <- debería ser si fuera pura
+
+console.log(datos); // []
